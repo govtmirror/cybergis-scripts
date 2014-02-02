483e3625a992c04fb4872bea98d62dd8dcbd4a81
state-hiu <HIU_INFO@state.gov>
2014-02-02 01:07:30 -0500

    Added cybergis-script-pull-wfs.sh

---
+++ bin/cybergis-script-pull-wfs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+if [[ $# -ne 7 ]]; then
+	echo "Usage: cybergis-script-pull-wfs.sh wfs namespace featuretype dbname dbuser dbpass table"
+	exit
+fi
+DATE=$(date)
+FORMAT=json
+WFS=$1
+NAMESPACE=$2
+FEATURETYPE=$3
+DBNAME=$4
+DBUSER=$5
+DBPASS=$6
+TABLE=$7
+URL="$WFS?typename=$NAMESPACE%3A$FEATURETYPE&outputFormat=$FORMAT&version=1.0.0&request=GetFeature&service=WFS"
+TEMP=/tmp/cybergis-pull
+
+echo "URL: "$URL
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user" 2>&1
+  exit 1
+else
+	echo ""
+	echo "Starting pull at "$DATE
+	if [ -d $TEMP ] ; then
+		echo "Removing data from previous pull"
+		rm -fr $TEMP
+	fi
+
+	mkdir $TEMP
+	
+	cd $TEMP
+
+	echo "Retreiving data from "$URL
+	wget $URL -O pull.geojson
+	ogr2ogr -overwrite -a_srs EPSG:900913 -f "PostgreSQL" PG:"host=localhost user=$DBUSER dbname=$DBNAME password=$DBPASS" pull.geojson -nln "$TABLE"
+	echo "Pull completed"
+fi
