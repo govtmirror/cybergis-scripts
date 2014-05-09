951bbecd9d74ce3a6f13efb5c8ec9e3ca96537cb
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-05-09 17:01:44 -0400

    Update cybergis-script-init-rogue.sh

--- bin/cybergis-script-init-rogue.sh
+++ bin/cybergis-script-init-rogue.sh
@@ -77,9 +77,10 @@ add_server(){
           #JSON="{\\\\"source\\\\":{\\\\"ptype\\\\":\\\\"gxp_tmssource\\\\",\\\\"name\\\\":\\\\"$NAME\\\\",\\\\"url\\\\":\\\\"$URL\\\\"},\\\\"visibility\\\\":True}"
           JSON='{\\"source\\":{\\"ptype\\":\\"gxp_tmssource\\",\\"name\\":\\"'$NAME'\\",\\"url\\":\\"'$URL'\\"},\\"visibility\\":True}'
           LINE="MAP_BASELAYERS.append($JSON)"
+          CMD='echo "'$LINE'" >> "'$FILE_SETTINGS'"'
           #echo $LINE
           #echo $FILE_SETTINGS
-          bash --login -c "echo \""$LINE"\" >> \"$FILE_SETTINGS\""
+          bash --login -c $CMD
       else
           echo "Usage: cybergis-script-init-rogue.sh $INIT_ENV $INIT_CMD [tms] <name> <url>"
       fi
