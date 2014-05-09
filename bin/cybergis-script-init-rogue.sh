7d0325f708cb7cceb889f912b5081d0dd663ad3b
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-05-08 22:48:14 -0400

    Update cybergis-script-init-rogue.sh

--- bin/cybergis-script-init-rogue.sh
+++ bin/cybergis-script-init-rogue.sh
@@ -50,7 +50,7 @@ install_geonode(){
   sed -i "s/dev.rogue.lmnsolutions.com/$FQDN/g" dna.json
   chmod 755 run.sh
   #
-  #bash --login run.sh
+  bash --login run.sh
 }
 
 if [[ "$INIT_ENV" = "prod" ]]; then
