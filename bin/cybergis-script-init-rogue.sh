fa118a0c4fa49341513d69654d9db9403abd20e8
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-05-08 23:10:46 -0400

    Update cybergis-script-init-rogue.sh

--- bin/cybergis-script-init-rogue.sh
+++ bin/cybergis-script-init-rogue.sh
@@ -4,13 +4,16 @@
 #Run this script using rogue's login shell under: sudo su - <user>
 
 if [[ $# -ne 2 ]]; then
-	echo "Usage: cybergis-script-init-rogue.sh [prod|dev] [user|rvm|gems|geonode]"
+	echo "Usage: cybergis-script-init-rogue.sh [prod|dev] [user|rvm|gems|geonode|remotes]"
 	exit
 fi
 
 DATE=$(date)
 RUBY_VERSION="2.0.0-p353"
-FQDN="hiu-maps.net"
+FQDN="example.com"
+
+CTX_GEOGIT="/geoserver/geogit/"
+
 
 INIT_ENV=$1
 INIT_CMD=$2
@@ -41,16 +44,29 @@ install_gems(){
 
 install_geonode(){
   echo "install_geonode"
-  #
-  cd /opt
-  git clone https://github.com/ROGUE-JCTD/rogue-chef-repo.git
-  mkdir chef-run
-  cp -r /opt/rogue-chef-repo/solo/* chef-run/
-  cd chef-run
-  sed -i "s/dev.rogue.lmnsolutions.com/$FQDN/g" dna.json
-  chmod 755 run.sh
-  #
-  bash --login run.sh
+  if [[ "$FQDN" != "example.com" ]]; then
+    #
+    cd /opt
+    git clone https://github.com/ROGUE-JCTD/rogue-chef-repo.git
+    mkdir chef-run
+    cp -r /opt/rogue-chef-repo/solo/* chef-run/
+    cd chef-run
+    sed -i "s/dev.rogue.lmnsolutions.com/$FQDN/g" dna.json
+    chmod 755 run.sh
+    #
+    bash --login run.sh
+  else
+    echo "You need to set the FQDN variable before continuing."
+  fi
+}
+
+init_remote(){
+  if [[ $# -ne 5 ]]; then
+    echo "Usage: init_remotes <user> <password> <repo_name> <remote_name> <remote_url>"
+    return
+  fi
+  URL = "http://$FQDN$CTX_GEOGIT$1/remote?user=$1&password=$2&output_format=JSON&remoteName=$3&remoteURL=$4"
+  echo $URL
 }
 
 if [[ "$INIT_ENV" = "prod" ]]; then
@@ -74,6 +90,11 @@ if [[ "$INIT_ENV" = "prod" ]]; then
         export -f install_geonode
         bash --login -c install_geonode
     fi
+    
+    #if [[ "$INIT_CMD" == "remote" ]]; then
+    #    export -f init_remote
+    #    bash --login -c init_remote
+    #fi
     
 fi
 
