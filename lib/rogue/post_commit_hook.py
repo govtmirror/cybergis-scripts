5c82b8f1b550d37922fc65da195d86a4a9a870d9
state-hiu <HIU_INFO@state.gov>
2014-05-14 01:18:59 -0400

    minor fix

--- lib/rogue/post_commit_hook.py
+++ lib/rogue/post_commit_hook.py
@@ -4,14 +4,14 @@ import boto
 
 from django.conf import settings
 
-log = open("/var/log/tomcat/post_commit_hook_2.out","w")
+log = open("/var/log/tomcat7/post_commit_hook_2.out","w")
 log.write("Log 2 opened")
 
 if len(sys.argv) == 2:
     msg_geogit = sys.argv[1]
     sns = boto.connect_sns(aws_access_key_id=settings.AWS_ACCESS_KEY_ID,aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
     topic = settings.AWS_SNS_TOPIC
-    if (not (topic is non)) and (len(topic)>0):
+    if (not (topic is None)) and (len(topic)>0):
         msg_sns = "User ___ just commited an update to the ____ GeoGit Repo on the ____ GeoNode at ______.\n\nThe commit message was:\n\n"+msg_geogit+"\n\nThis message was automatically generated.  Please contact the system administrator of the specified GeoNode to adjust your preferences."
 
         sns = boto.connect_sns(aws_access_key_id=id,aws_secret_access_key=key)
