d316a5deee33204acf928e1c9aaa4f6611c0ce54
state-hiu <HIU_INFO@state.gov>
2014-05-13 23:04:09 -0400

    added in ROGUE post commit sns hook.py

---
+++ lib/rogue/post_commit_hook.py
@@ -0,0 +1,16 @@
+#!/var/lib/geonode/bin/python
+
+import boto
+
+from django.conf import settings
+
+sns = boto.connect_sns(aws_access_key_id=settings.AWS_ACCESS_KEY_ID,aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+topic = settings.AWS_SNS_TOPIC
+if (not (topic is non)) and (len(topic)>0):
+    msg_geogit = params.get("message");
+    msg_sns = "User ___ just commited an update to the ____ GeoGit Repo on the ____ GeoNode at ______.\n\nThe commit message was:\n\n"+msg_geogit+"\n\nThis message was automatically generated.  Please contact the system administrator of the specified GeoNode to adjust your preferences."
+
+    sns = boto.connect_sns(aws_access_key_id=id,aws_secret_access_key=key)
+    res = sns.publish(topic, msg_sns)
+else:
+    print "Topic was invalid"
