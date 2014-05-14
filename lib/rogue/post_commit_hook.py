228568c99cb0008c4fc6a5fb52b9f5399c6510e1
state-hiu <HIU_INFO@state.gov>
2014-05-14 01:22:41 -0400

    minor fix

--- lib/rogue/post_commit_hook.py
+++ lib/rogue/post_commit_hook.py
@@ -14,7 +14,6 @@ if len(sys.argv) == 2:
     if (not (topic is None)) and (len(topic)>0):
         msg_sns = "User ___ just commited an update to the ____ GeoGit Repo on the ____ GeoNode at ______.\n\nThe commit message was:\n\n"+msg_geogit+"\n\nThis message was automatically generated.  Please contact the system administrator of the specified GeoNode to adjust your preferences."
 
-        sns = boto.connect_sns(aws_access_key_id=id,aws_secret_access_key=key)
         res = sns.publish(topic, msg_sns)
     else:
         log.write( "Topic was invalid")
