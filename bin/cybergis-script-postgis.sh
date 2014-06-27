1ab22b937a5d0596a8dd4e732265ef51a440838d
state-hiu <HIU_INFO@state.gov>
2014-06-27 14:02:31 -0400

    postgis install on rds

--- bin/cybergis-script-postgis.sh
+++ bin/cybergis-script-postgis.sh
@@ -12,18 +12,27 @@ INIT_CMD=$2
 
 install(){
   echo "install"
-  if [[ $# -ne 4 ]]; then
-    echo "Usage: cybergis-script-postgis prod install <host> <port> <user> <password>"
+  if [[ $# -ne 9 ]]; then
+    echo "Usage: cybergis-script-postgis.sh prod install [rds|local] <host> <port> <user> <password> <database> <template>"
   else
     #
     INIT_ENV=$1
     INIT_CMD=$2
-    HOST=$3
-    PORT=$4
-    USER=$5
-    PASS=$6
+    TYPE=$3
+    HOST=$4
+    PORT=$5
+    USER=$6
+    PASS=$7
+    DATABASE=$8 #likely template_postgis
+    TEMPLATE=$9 #likely tempalte0
     #
-    PGPASSWORD=$PASS psql --host=$HOST --port=$PORT --username $USER --password -f lib/postgis/postgis_install.sql
+    CMD_1="DATABASE "$DATABASE" ENCODING 'UTF8' TEMPLATE "$TEMPLATE";"
+    PGPASSWORD=$PASS psql --host=$HOST --port=$PORT --username $USER --password -c "$CMD_1"
+    #PGPASSWORD=$PASS psql --host=$HOST --port=$PORT --username $USER --password -d $DATABASE -f lib/postgis/postgis_install.sql
+    if [[ "$TYPE" = "rds" ]]; then
+      #PGPASSWORD=$PASS psql --host=$HOST --port=$PORT --username $USER --password -d $DATABASE -f lib/postgis/postgis_install_rds.sql
+      echo "skipping rds install"
+    fi
   fi
 }
 
@@ -32,10 +41,10 @@ if [[ "$INIT_ENV" = "prod" ]]; then
     if [[ "$INIT_CMD" == "install" ]]; then
         
         if [[ $# -ne 4 ]]; then
-            echo "Usage: cybergis-script-postgis.sh $INIT_ENV $INIT_CMD <host> <user> <password> <dbname>"
+            echo "Usage: cybergis-script-postgis.sh $INIT_ENV $INIT_CMD <host> <port> <user> <password> <database> <template>"
         else
             export -f tune
-            bash --login -c "tune $INIT_ENV $INIT_CMD \"$3\" \"$4\""
+            bash --login -c "tune $INIT_ENV $INIT_CMD \"$3\" \"$4\" \"$5\" \"$6\" \"$7\" \"$8\""
         fi
     else
         echo "Usage: cybergis-script-postgis.sh prod [install]"
