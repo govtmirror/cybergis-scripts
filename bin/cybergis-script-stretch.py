3f7f0229f6158fe52b98591a4605a566ef79d95a
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-07-16 13:05:19 -0400

    Update cybergis-script-stretch.py

--- bin/cybergis-script-stretch.py
+++ bin/cybergis-script-stretch.py
@@ -342,18 +342,18 @@ def main():
 								for y in range(inBand.YSize%r):
 									outBand.WriteArray(lut[inBand.ReadAsArray(0,y0+y,inBand.XSize,1,inBand.XSize,1)],0,y0+y)
 						elif numberOfThreads > 1:
-                            print "not fully implemented yet"
-                            global exitFlag
-                            global queueLock
-                            global writeLock
-                            global workQueue
-                            global tasks
+							print "not fully implemented yet"
+							global exitFlag
+							global queueLock
+							global writeLock
+							global workQueue
+							global tasks
 							#
-                            exitFlag = 0
-                            queueLock = Lock()
-                            writeLock = Lock()
-                            workQueue = Queue(0)
-                            tasks = Tasks()
+							exitFlag = 0
+							queueLock = Lock()
+							writeLock = Lock()
+							workQueue = Queue(0)
+							tasks = Tasks()
 							#
 							for b in range(inputBands):
 								print "Adding tasks for band "+str(b+1)
