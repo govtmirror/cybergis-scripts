6709615086c7e2a219c2d40ff94f83e2c490cf62
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-07-15 22:18:42 -0400

    Update cybergis-script-env.sh

--- bin/cybergis-script-env.sh
+++ bin/cybergis-script-env.sh
@@ -16,26 +16,32 @@ geonode(){
   else
     ENV=$1
     #
-    apt-get update
+    sudo apt-get update
     # Essential build tools and libraries
-    apt-get install -y build-essential libxml2-dev libxslt1-dev libjpeg-dev gettext git python-dev python-pip
+    sudo apt-get install -y build-essential libxml2-dev libxslt1-dev libjpeg-dev gettext git python-dev python-pip python-virtualenv
     # Python and Django dependencies with official packages
-    apt-get install -y  python-lxml python-psycopg2 python-django python-bs4 python-multipartposthandler transifex-client python-nose python-django-nose python-gdal python-django-pagination python-django-extensions python-httplib2
+    sudo apt-get install -y python-lxml python-psycopg2 python-django python-bs4 python-multipartposthandler transifex-client python-nose python-django-nose python-gdal python-django-pagination python-django-extensions python-httplib2
     # Java dependencies
-    apt-get install -y --force-yes openjdk-6-jdk ant maven2 --no-install-recommends
-
-    pip install python-pillow python-paver python-django-taggit python-django-jsonfield 
+    sudo apt-get install -y --force-yes openjdk-6-jdk ant maven2 --no-install-recommends
+    #Install python packages for development
+    sudo pip install virtualenvwrapper paver
     
-
     #Add all these lines to ~/.bash_aliases
-    echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
-export WORKON_HOME=~/.venvs
-source /usr/local/bin/virtualenvwrapper.sh
-export PIP_DOWNLOAD_CACHE=$HOME/.pip-downloads
-workon geonode
-"
+    echo 'export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python' >> ~/.bash_alises
+    echo 'export WORKON_HOME=~/.venvs' >> ~/.bash_alises
+    echo 'source /usr/local/bin/virtualenvwrapper.sh'>> ~/.bash_alises
+    echo 'export PIP_DOWNLOAD_CACHE=$HOME/.pip-downloads' >> ~/.bash_alises
+    echo 'workon geonode' >> ~/.bash_alises
     
-
+    cd ~/geonode
+    mkvirtualenv geonode
+    workon geonode
+    pip install pillow django-taggit django-jsonfield
+    #Install GeoNode
+    pip install -e geonode
+    #cd geonode
+    #./restart.sh
+    
   fi
 }
 
