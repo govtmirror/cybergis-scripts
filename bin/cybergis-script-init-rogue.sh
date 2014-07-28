99571640051eea140fe5ba07a34407fb8be8dd05
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-07-28 17:40:28 -0400

    Update cybergis-script-init-rogue.sh

--- bin/cybergis-script-init-rogue.sh
+++ bin/cybergis-script-init-rogue.sh
@@ -380,7 +380,7 @@ osm(){
               #===============#
               echo post_geogitdatastore.xml
               REST_DATASTORES="http://localhost/geoserver/rest/workspaces/geonode/datastores.xml"
-              #curl $REST_DATASTORES -u $USERPASS -H 'Content-type: xml' -XPOST -d @data.xml
+              curl $REST_DATASTORES -u $USERPASS -H 'Content-type: xml' -XPOST -d @data.xml
               #===============#
               ##Add GeoGit Datastore to Tomcat
               #Add Layer to Tomcat
