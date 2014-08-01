ff5622b6adc0b559ea1b54458d6faa9b6989890d
state-hiu <HIU_INFO@state.gov>
2014-08-01 18:26:29 -0400

    refractoring conf_standalone top support banner

--- bin/cybergis-script-init-rogue.sh
+++ bin/cybergis-script-init-rogue.sh
@@ -89,7 +89,7 @@ conf_application(){
 }
 
 conf_standalone(){
-  if [ [ $# -ne 4 ] && [ $# -ne 7] ]; then
+  if [ $# -ne 4 ] && [ $# -ne 7]; then
     echo "Usage: cybergis-script-init-rogue.sh prod conf_standalone <fqdn> <gs_baseline>"
     echo "or"
     echo "Usage: cybergis-script-init-rogue.sh prod conf_standalone <fqdn> <gs_baseline> <banner_text> <banner_color_text> <banner_color_background>"
