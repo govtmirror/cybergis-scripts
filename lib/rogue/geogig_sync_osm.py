ee3fa61b5aab046ff9a581ccde33a17baeafe218
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-10-09 16:04:45 -0400

    Update geogig_sync_osm.py

--- lib/rogue/geogig_sync_osm.py
+++ lib/rogue/geogig_sync_osm.py
@@ -5,17 +5,6 @@ import urllib
 import urllib2
 import argparse
 
-parser = argparse.ArgumentParser(description='Synchronize GeoGig repository with OpenStreetMap (OSM)')
-parser.add_argument("url", help="The url to the repository you want to sync.")
-parser.add_argument("--username", help="The username to use for basic auth requests.")
-parser.add_argument("--password", help="The password to use for basic auth requests.")
-parser.add_argument("authorname", help="The author name to use when merging non-conflicting branches.")
-parser.add_argument("authoremail", help="The author email to use when merging non-conflicting branches.")
-args = parser.parse_args()
-
-print args.accumulate(args.integers)
-
-
 def beginTransaction(url, auth):
     params = {'output_format': 'JSON'}
     self.stdout.write('Starting transaction...')
@@ -102,6 +91,13 @@ def run(args):
         endTransaction(url, auth, False, transactionId)
     except Exception:
         pass
-        
+
+parser = argparse.ArgumentParser(description='Synchronize GeoGig repository with OpenStreetMap (OSM)')
+parser.add_argument("url", help="The url to the repository you want to sync.")
+parser.add_argument("--username", help="The username to use for basic auth requests.")
+parser.add_argument("--password", help="The password to use for basic auth requests.")
+parser.add_argument("authorname", help="The author name to use when merging non-conflicting branches.")
+parser.add_argument("authoremail", help="The author email to use when merging non-conflicting branches.")
+  
 args = parser.parse_args()
 run(args)
