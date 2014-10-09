de69f623208b1df5a006bced9de614ff2178f8a6
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-10-09 16:08:19 -0400

    Update geogig_sync_osm.py

--- lib/rogue/geogig_sync_osm.py
+++ lib/rogue/geogig_sync_osm.py
@@ -96,8 +96,8 @@ parser = argparse.ArgumentParser(description='Synchronize GeoGig repository with
 parser.add_argument("url", help="The url to the repository you want to sync.")
 parser.add_argument("--username", help="The username to use for basic auth requests.")
 parser.add_argument("--password", help="The password to use for basic auth requests.")
-parser.add_argument("authorname", help="The author name to use when merging non-conflicting branches.")
-parser.add_argument("authoremail", help="The author email to use when merging non-conflicting branches.")
+parser.add_argument("--authorname", help="The author name to use when merging non-conflicting branches.")
+parser.add_argument("--authoremail", help="The author email to use when merging non-conflicting branches.")
   
 args = parser.parse_args()
 run(args)
