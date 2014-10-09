1d0a981fc082995ba75fcf8efc127b761e8f4379
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-10-09 15:41:13 -0400

    Update geogig_sync_osm.py

--- lib/rogue/geogig_sync_osm.py
+++ lib/rogue/geogig_sync_osm.py
@@ -7,8 +7,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Synchronize GeoGig repository with OpenStreetMap (OSM)')
 parser.add_argument("url", help="The url to the repository you want to sync.")
-parser.add_argument("username", help="The username to use for basic auth requests.")
-parser.add_argument("password", help="The password to use for basic auth requests.")
+parser.add_argument("--username", help="The username to use for basic auth requests.")
+parser.add_argument("--password", help="The password to use for basic auth requests.")
 parser.add_argument("authorname", help="The author name to use when merging non-conflicting branches.")
 parser.add_argument("authoremail", help="The author email to use when merging non-conflicting branches.")
 args = parser.parse_args()
