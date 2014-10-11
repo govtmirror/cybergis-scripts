339918ba13cd0bcf733294e7bbeee338cfc199e9
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-10-11 18:24:25 -0400

    Update geogig_sync_osm.py

--- lib/rogue/geogig_sync_osm.py
+++ lib/rogue/geogig_sync_osm.py
@@ -16,7 +16,7 @@ parser.add_argument('-gs', '--geoserver', help="The url of the GeoServer servici
 parser.add_argument('-ws', '--workspace', help="The GeoServer workspace to use for the data store.")
 #
 parser.add_argument('-ds', '--datastore', help="The name of the GeoServer data store of the GeoGig repository you want to sync.")
-parser.add_argument("--repo", help="The GeoServer id of the GeoGig repository you want to sync.")
+parser.add_argument('-r', '--repo', help="The GeoServer id of the GeoGig repository you want to sync.")
 
 parser.add_argument("--username", help="The username to use for basic auth requests.")
 parser.add_argument("--password", help="The password to use for basic auth requests.")
@@ -24,7 +24,7 @@ parser.add_argument("--authorname", help="The author name to use when merging no
 parser.add_argument("--authoremail", help="The author email to use when merging non-conflicting branches.")
 parser.add_argument("--extent", help="The extent of the OpenStreetMap extract. For example, basic:buildings_and_roads.")
 parser.add_argument("--mapping", help="The mapping of the OpenStreetMap extract.  For example, dominican_republic:santo_domingo.")
-parser.add_argument("--timeout", type=int, default=30, help="The number of seconds to wait for the osm download task to complete before cancelling.  Default is 30 seconds.")
+parser.add_argument('-to', '--timeout', type=int, default=30, help="The number of seconds to wait for the osm download task to complete before cancelling.  Default is 30 seconds.")
 parser.add_argument('--verbose', '-v', default=0, action='count', help="Print out intermediate status messages.")
 args = parser.parse_args()
 #==#
