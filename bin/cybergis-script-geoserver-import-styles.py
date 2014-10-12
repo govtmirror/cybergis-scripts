6eb2dd294ffc3459ed8f73ca9b6ac29f6738455a
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-10-12 13:31:26 -0400

    Update cybergis-script-geoserver-import-styles.py

--- bin/cybergis-script-geoserver-import-styles.py
+++ bin/cybergis-script-geoserver-import-styles.py
@@ -10,7 +10,7 @@ import os
 import subprocess
 #==#
 #sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib')))
-from ..lib import cybergis.gs._geoserver_import_styles
+from ..lib.cybergis.gs import _geoserver_import_styles
 #==#
 parser = argparse.ArgumentParser(description='')
 parser.add_argument("--path", help="The location in the filesystem of the styles directory")
