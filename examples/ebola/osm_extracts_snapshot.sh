82e442e4813a8ce440d332030f56dab5709fde01
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-10-12 18:37:04 -0400

    Update osm_extracts_snapshot.sh

--- examples/ebola/osm_extracts_snapshot.sh
+++ examples/ebola/osm_extracts_snapshot.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-#==#
+#==========##========#
+BIN=/opt/cybergis-scripts.git/bin
 TIMESTAMP=$(date +%s)
 #==#
-WFS='http://example.com:8080/geoserver/wfs'
+USER=admin
+PASS=geoserver
+#==#
+GS='http://localhost:8080/geoserver/'
+#==#
+WFS=$GS"wfs"
 HOST='<Database Host>'
 DB=<Database Name>
 USER='postgres'
 PASS=<Database Password>
 NS=osm-extracts
 PRJ='EPSG:4326'
+#==#
+#For publishing
+WS=<Workspace>
+DS=<Data Store>
 #===================#
-FTA=( "monrovia_basic_osm_buildings" "kenema_basic_osm_buildings" )
+FTA=( "monrovia_basic_osm_buildings" "kenema_basic_osm_buildings")
 for FT in "${FTA[@]}"
 do
-    cybergis-script-pull-wfs.sh $WFS $NS $FT $PRJ $HOST $DB $USER $PASS $NS"_"$FT"_"$TIMESTAMP
+    SNAP=$NS"_"$FT"_"$TIMESTAMP
+    echo "-----------"
+    echo "Snapshoting "$FT" as "$NS"_"$FT"_"$TIMESTAMP
+    echo "Snapshoting"
+    $BIN/cybergis-script-pull-wfs.sh $WFS $NS $FT $PRJ $HOST $DB $USER $PASS $SNAP
+    echo "Publishing"
+    $BIN/cybergis-script-geoserver-publish-layers.py -gs $GS -ws $WS -ds $DS -ft $SNAP --username $USER --password $PASS
 done
+#===================#
