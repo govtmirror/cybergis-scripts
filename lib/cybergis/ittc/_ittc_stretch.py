a06b1a559485a3d2ad7c88f9fd1d91cb80000ff4
state-hiu <HIU_INFO@state.gov>
2014-10-12 15:55:28 -0400

    fixed idents

--- lib/cybergis/ittc/_ittc_stretch.py
+++ lib/cybergis/ittc/_ittc_stretch.py
@@ -19,16 +19,17 @@ workQueue = None
 tasks = None
 
 class Tasks(object):
-	def __init__(self):
-		self.tasks = []
-	
-	def add(self,task):
-		self.tasks.append(task)
-	
-	def get(self,task):
-		return self.tasks[task]
+    def __init__(self):
+        self.tasks = []
+
+    def add(self,task):
+        self.tasks.append(task)
+
+    def get(self,task):
+        return self.tasks[task]
 		
 class RenderSubprocess(object):
+
     def __init__(self, processID, processName, queue, tasks):
         self.processID = processID
         self.processName = processName
@@ -41,269 +42,271 @@ class RenderSubprocess(object):
         self.tries = 3
         
     def run(self):
-    	while not exitFlag:
-    		if self.strip is None:
-    			queueLock.acquire()
-        		if not workQueue.empty():
-            			b, inBand, outBand, lut, y0, y, r, t = self.task = self.tasks.get(self.queue.get())
-            			queueLock.release()
-            			#==#
-            			if t==1:
-			            	#print self.processName+" reading rows "+str(y*r)+" to "+str((y*r)+r-1)+" in band "+str(b)+"."
-            				try:
-            					self.strip = inBand.ReadAsArray(0,y*r,inBand.XSize,r,inBand.XSize,r)
-            					self.tries = 10
-            				except:
-            					self.strip = None
-            					self.strip_stretched = None
-            					print "read failed.  will try again later."
-            					print "y:"+str(y)
-            					print "r:"+str(r)
-            				
-            			elif t==2:
-			            	#print self.processName+" reading row "+str((y0*r)+y)+" in band "+str(b)+"."
-            				try:
-            					self.strip = inBand.ReadAsArray(0,(y0*r)+y,inBand.XSize,1,inBand.XSize,1)
-            					self.tries = 10
-            				except:
-            					self.strip = None
-            					self.strip_stretched = None
-            					print "read failed.  will try again later."
-            					print "y0:"+str(y0)
-            					print "y:"+str(y)
-            					print "r:"+str(r)
-            					print "(y0*r)+y:"+str((y0*r)+y)
-            				
-        		else:
-            			queueLock.release()
-            	elif (not self.strip is None) and (self.strip_stretched is None):
-            		b, inBand, outBand, lut, y0, y, r, t = self.task
-            		self.strip_stretched = lut[self.strip]
-        	elif (not self.strip is None) and (not self.strip_stretched is None):
-        		if self.tries > 0:
-        			b, inBand, outBand, lut, y0, y, r, t = self.task
-        			writeLock.acquire()
-        			if t==1:
-			            	#print self.processName+" writing rows "+str(y*r)+" to "+str((y*r)+r-1)+" in band "+str(b)+"."
-            				try:
-	            				outBand.WriteArray(self.strip_stretched,0,y*r)
-            					self.strip = None
-            					self.strip_stretched = None
-            				except:
-	            				self.tries = self.tries - 1
-            					print "write failed.  "+str(self.tries)+" more tries."
-            			elif t==2:
-			            	#print self.processName+" writing row "+str((y0*r)+y)+" in band "+str(b)+"."
-            				try:
-            					outBand.WriteArray(self.strip_stretched,0,(y0*r)+y)
-            					self.strip = None
-            					self.strip_stretched = None
-            				except:
-            					self.tries = self.tries -1
-            					print "write failed.  "+str(self.tries)+" more tries."
-            			writeLock.release()
-            		else:
-            			print "abandoning write"
-            			self.strip = None
-            			self.strip_stretched = None
-            	else:
-            	    print "abandoning task.  This should never happen."
-            	    self.strip = None
-            	    self.strip_stretched = None
-            	    
-        	time.sleep(1)
+        while not exitFlag:
+    	    if self.strip is None:
+                queueLock.acquire()
+                if not workQueue.empty():
+                    b, inBand, outBand, lut, y0, y, r, t = self.task = self.tasks.get(self.queue.get())
+                    queueLock.release()
+                    #==#
+                    if t==1:
+                        #print self.processName+" reading rows "+str(y*r)+" to "+str((y*r)+r-1)+" in band "+str(b)+"."
+                        try:
+                            self.strip = inBand.ReadAsArray(0,y*r,inBand.XSize,r,inBand.XSize,r)
+                            self.tries = 10
+                        except:
+                            self.strip = None
+                            self.strip_stretched = None
+                            print "read failed.  will try again later."
+                            print "y:"+str(y)
+                            print "r:"+str(r)
+
+                    elif t==2:
+                        #print self.processName+" reading row "+str((y0*r)+y)+" in band "+str(b)+"."
+                        try:
+                            self.strip = inBand.ReadAsArray(0,(y0*r)+y,inBand.XSize,1,inBand.XSize,1)
+                            self.tries = 10
+                        except:
+                            self.strip = None
+                            self.strip_stretched = None
+                            print "read failed.  will try again later."
+                            print "y0:"+str(y0)
+                            print "y:"+str(y)
+                            print "r:"+str(r)
+                            print "(y0*r)+y:"+str((y0*r)+y)
+
+                else:
+                    queueLock.release()
+
+            elif (not self.strip is None) and (self.strip_stretched is None):
+                b, inBand, outBand, lut, y0, y, r, t = self.task
+                self.strip_stretched = lut[self.strip]
+            elif (not self.strip is None) and (not self.strip_stretched is None):
+                if self.tries > 0:
+                    b, inBand, outBand, lut, y0, y, r, t = self.task
+                    writeLock.acquire()
+                    if t==1:
+                        #print self.processName+" writing rows "+str(y*r)+" to "+str((y*r)+r-1)+" in band "+str(b)+"."
+                        try:
+                            outBand.WriteArray(self.strip_stretched,0,y*r)
+                            self.strip = None
+                            self.strip_stretched = None
+                        except:
+                            self.tries = self.tries - 1
+                            print "write failed.  "+str(self.tries)+" more tries."
+                    elif t==2:
+                        #print self.processName+" writing row "+str((y0*r)+y)+" in band "+str(b)+"."
+                        try:
+                            outBand.WriteArray(self.strip_stretched,0,(y0*r)+y)
+                            self.strip = None
+                            self.strip_stretched = None
+                        except:
+                            self.tries = self.tries -1
+                            print "write failed.  "+str(self.tries)+" more tries."
+                    writeLock.release()
+                else:
+                    print "abandoning write"
+                    self.strip = None
+                    self.strip_stretched = None
+
+            else:
+                print "abandoning task.  This should never happen."
+                self.strip = None
+                self.strip_stretched = None
+
+            time.sleep(1)
 
 def execute(subprocess):
-	subprocess.run()
+    subprocess.run()
 
 class BreakPoint:
 
-	def __init__(self, i = None, o = None):
-		self.i = i #Should be double; even though it is most frequently an int
-		self.o = o
+    def __init__(self, i = None, o = None):
+        self.i = i #Should be double; even though it is most frequently an int
+        self.o = o
+
+class BreakPoints:
+
+    def __init__(self):
+        self.bps = []
+
+    def add(self,bp):
+        self.bps.append(bp)
+
+    def extend(self,bps):
+        self.bps.extend(bps)
 
 class Line:
 
-	def __init__(self, b0, b1):
-		self.x0 = b0.i
-		self.y0 = b0.o
-		self.x1 = b1.i
-		self.y1 = b1.o
-		self.n = float(self.y1-self.y0)
-		self.d = float(self.x1-self.x0)
-		if self.d != 0:
-			self.m = self.n/self.d
-			self.b = self.y0 - self.m*self.x0
-		else:
-			self.m = None
-			self.b = None
+    def __init__(self, b0, b1):
+        self.x0 = b0.i
+        self.y0 = b0.o
+        self.x1 = b1.i
+        self.y1 = b1.o
+        self.n = float(self.y1-self.y0)
+        self.d = float(self.x1-self.x0)
+        if self.d != 0:
+            self.m = self.n/self.d
+            self.b = self.y0 - self.m*self.x0
+        else:
+            self.m = None
+            self.b = None
 
-	def min(self):
-		return self.x0
-	
-	def max(self):
-		return self.x1
+        def min(self):
+            return self.x0
 
-	def contains(self, x):
-		return x>=self.x0 and x <=self.x1
+        def max(self):
+            return self.x1
 
-	def calc(self, x):
-		if self.d != 0:
-			return int(round(self.m*x+self.b))
-		elif self.d == 0 and self.y1 == self.y0:
-			return self.y1
-		else:
-			return x
+        def contains(self, x):
+            return x>=self.x0 and x <=self.x1
+
+        def calc(self, x):
+            if self.d != 0:
+                return int(round(self.m*x+self.b))
+            elif self.d == 0 and self.y1 == self.y0:
+                return self.y1
+            else:
+                return x
 
 class Lines:
 
-	def __init__(self, bps):
-		self.lines = []
-		for i in range(len(bps)-1):
-			if (not (bps[i] is None)) and (not (bps[i+1] is None)):
-				self.lines.append(Line(bps[i],bps[i+1]))
+    def __init__(self, bps):
+        self.lines = []
+        for i in range(len(bps)-1):
+            if (not (bps[i] is None)) and (not (bps[i+1] is None)):
+                self.lines.append(Line(bps[i],bps[i+1]))
 	
-	def size(self):
-		return len(self.lines)	
+    def size(self):
+        return len(self.lines)	
 
-	def min(self):
-		return self.lines[0].min()
-	
-	def max(self):
-		return self.lines[len(self.lines)-1].max()
+    def min(self):
+        return self.lines[0].min()
 
-	def calc(self, x):
-		y = x
-		for line in self.lines:
-			if line.contains(x):
-				y = line.calc(x)
-				break
-		return y
+    def max(self):
+        return self.lines[len(self.lines)-1].max()
+
+    def calc(self, x):
+        y = x
+        for line in self.lines:
+            if line.contains(x):
+                y = line.calc(x)
+                break
+        return y
 
 class LookUpTable:
-	
-	def __init__(self,bps):
-		self.lines = Lines(bps)
-		self.table = []
-		self.min = int(self.lines.min())
-		self.max = int(self.lines.max())
-		
-		for i in range(0,256):
-			self.table.append(self.lines.calc(i))
+
+    def __init__(self,bps):
+        self.lines = Lines(bps)
+        self.table = []
+        self.min = int(self.lines.min())
+        self.max = int(self.lines.max())
+
+        for i in range(0,256):
+            self.table.append(self.lines.calc(i))
 		
 class LookUpTable2:
 
-	def __init__(self,bps_red,bps_green,bps_blue):
-		self.lines_red = Lines(bps_red)
-		self.lines_green = Lines(bps_green)
-		self.lines_blue = Lines(bps_blue)
-		self.table = []
-		for i in range(0,256):
-			self.table.append(int((self.lines_red.calc(i)+self.lines_green.calc(i)+self.lines_blue.calc(i))/3.0))
+    def __init__(self,bps_red,bps_green,bps_blue):
+        self.lines_red = Lines(bps_red)
+        self.lines_green = Lines(bps_green)
+        self.lines_blue = Lines(bps_blue)
+        self.table = []
+        for i in range(0,256):
+            self.table.append(int((self.lines_red.calc(i)+self.lines_green.calc(i)+self.lines_blue.calc(i))/3.0))
 
 class LookUpTables:
 
-	def __init__ (self, filename):
-		self.bps_red = None
-		self.bps_green = None
-		self.bps_blue = None
-		self.tables = None
-		self.table = None
+    def __init__ (self, filename, numberOfBands):
+        self.bps_list = []
+        self.tables = None
+        self.table = None
+        self.file = None
+        self.valid = True
+        self.init_breakpoints(filename,numberOfBands)
+        self.init_tables(numberOfBands)
 
-		self.file = None
-		self.valid = True
+    def init_breakpoints(self,filename,numberOfBands):
+        self.bps_list = []
+        for i in range(numberOfBands):
+            self.bps_list.append(BreakPoints())
 
-		self.init_breakpoints(filename)
-		self.init_tables()
+        if filename.endswith(".cbp") or filename.endswith(".txt"):
+            lines = None
+            file = open(filename,"r")
+            with open(filename) as f:
+                lines = f.readlines()
 
-	def init_breakpoints(self,filename):
-                self.bps_red = []
-                self.bps_green = []
-                self.bps_blue = []
+            band = -1
+            for line in lines:
+                line = line.strip()
+                if line=="":
+                    continue
+                elif line=="rgbversion8.3":
+                    continue
+                elif line.startswith("#"):
+                    continue
+                elif line=="RGB":
+                    band = band+1
+                elif line.startswith("Band"):
+                    band = int(line.split()[1].strip(' \t\n\r'))-1
+                elif line.startswith("Break") or line.startswith("-"):
+                    self.bps_list[band].add(None)
+                else:
+                    terms = line.split()
+                    bp = BreakPoint(float(terms[0].strip(' \t\n\r')),float(terms[1].strip(' \t\n\r')))
+                    self.bps_list[band].add(bp)
 
-		if filename.endswith(".cbp") or filename.endswith(".txt"):
-			lines = None
-			file = open(filename,"r")
-			with open(filename) as f:
-				lines = f.readlines()
-			
-			band = -1
-			for line in lines:
-				line = line.strip()
-				if line=="":
-					continue
-				elif line=="rgbversion8.3":
-					continue
-				elif line.startswith("#"):
-					continue
-				elif line=="RGB":
-					band = band+1
-				elif line.startswith("Band"):
-					band = int(line.split()[1].strip(' \t\n\r'))-1
-				elif line.startswith("Break") or line.startswith("-"):
-					if band==0:
-						self.bps_red.append(None)
-					elif band==1:
-						self.bps_green.append(None)
-					elif band==2:
-						self.bps_blue.append(None)
-				else:
-					terms = line.split()
-					bp = BreakPoint(float(terms[0].strip(' \t\n\r')),float(terms[1].strip(' \t\n\r')))
-					if band==0:
-						self.bps_red.append(bp)
-					elif band==1:
-						self.bps_green.append(bp)
-					elif band==2:
-						self.bps_blue.append(bp)
+        elif filename.endswith(".qml"):
+            file = open(filename,"r")
+            dom = parse(file)
+            minRed = -1
+            maxRed = -1
+            minGreen = -1
+            maxGreen = -1
+            minBlue = -1
+            maxBlue = -1
+            for nRasterRenderer in dom.getElementsByTagName("rasterrenderer"):
+                for node in nRasterRenderer.childNodes:
+                    #print node.toxml()
+                    if node.nodeType != node.TEXT_NODE:
+                        if node.tagName=="redContrastEnhancement":
+                            minRed = float(node.getElementsByTagName("minValue")[0].firstChild.nodeValue)
+                            maxRed = float(node.getElementsByTagName("maxValue")[0].firstChild.nodeValue)
+                        elif node.tagName=="greenContrastEnhancement":
+                            minGreen = float(node.getElementsByTagName("minValue")[0].firstChild.nodeValue)
+                            maxGreen = float(node.getElementsByTagName("maxValue")[0].firstChild.nodeValue)
+                        elif node.tagName=="blueContrastEnhancement":
+                            minBlue = float(node.getElementsByTagName("minValue")[0].firstChild.nodeValue)
+                            maxBlue = float(node.getElementsByTagName("maxValue")[0].firstChild.nodeValue)
 
-		elif filename.endswith(".qml"):
-			file = open(filename,"r")
-			dom = parse(file)
-			minRed = -1
-			maxRed = -1
-			minGreen = -1
-			maxGreen = -1
-			minBlue = -1
-			maxBlue = -1
-			for nRasterRenderer in dom.getElementsByTagName("rasterrenderer"):
-				for node in nRasterRenderer.childNodes:
-					#print node.toxml()
-					if node.nodeType != node.TEXT_NODE:
-						if node.tagName=="redContrastEnhancement":
-							minRed = float(node.getElementsByTagName("minValue")[0].firstChild.nodeValue)
-							maxRed = float(node.getElementsByTagName("maxValue")[0].firstChild.nodeValue)
-						elif node.tagName=="greenContrastEnhancement":
-							minGreen = float(node.getElementsByTagName("minValue")[0].firstChild.nodeValue)
-							maxGreen = float(node.getElementsByTagName("maxValue")[0].firstChild.nodeValue)
-						elif node.tagName=="blueContrastEnhancement":
-							minBlue = float(node.getElementsByTagName("minValue")[0].firstChild.nodeValue)
-							maxBlue = float(node.getElementsByTagName("maxValue")[0].firstChild.nodeValue)
-			if minRed!=-1 and maxRed!=-1:
-				self.bps_red.extend(self.buildBreakPoints(minRed,maxRed))
-			if minGreen!=-1 and maxGreen!=-1:
-				self.bps_green.extend(self.buildBreakPoints(minGreen,maxGreen))
-                        if minBlue!=-1 and maxBlue!=-1:
-				self.bps_blue.extend(self.buildBreakPoints(minBlue,maxBlue))
-			
+            if minRed!=-1 and maxRed!=-1:
+                self.bps_list[0].extend(self.buildBreakPoints(minRed,maxRed))
+            if minGreen!=-1 and maxGreen!=-1:
+                self.bps_list[1].extend(self.buildBreakPoints(minGreen,maxGreen))
+            if minBlue!=-1 and maxBlue!=-1:
+                self.bps_list[2].extend(self.buildBreakPoints(minBlue,maxBlue))
 
-	def buildBreakPoints(self,minValue,maxValue):
-		bps = []
-		bps.append(BreakPoint(0.0,0.0))
-		bps.append(BreakPoint(minValue,0.0))
-		bps.append(BreakPoint(maxValue,255.0))
-		bps.append(BreakPoint(255.0,255.0))
-		return bps
-	
-	def init_tables(self):
-		self.tables = []
-		self.tables.append(LookUpTable(self.bps_red))
-		self.tables.append(LookUpTable(self.bps_green))
-		self.tables.append(LookUpTable(self.bps_blue))
-		self.tables.append(LookUpTable2(self.bps_red,self.bps_green,self.bps_blue))
 
-	def isValid(self):
-		return self.valid;
+    def buildBreakPoints(self,minValue,maxValue):
+        bps = []
+        bps.append(BreakPoint(0.0,0.0))
+        bps.append(BreakPoint(minValue,0.0))
+        bps.append(BreakPoint(maxValue,255.0))
+        bps.append(BreakPoint(255.0,255.0))
+        return bps
+
+    def init_tables(self,numberOfBands):
+        self.tables = []
+        if numberOfBands <= 0:
+            print "Error: number of Bands should be greater than 0".
+        else:
+            for b in range(numberOfBands):
+                self.tables.append(LookUpTable(self.bps_list[b].bps))
+                #self.tables.append(LookUpTable2(self.bps_list[0].bps,self.bps_green,self.bps_blue))
+
+    def isValid(self):
+        return self.valid;
 
 def initDataset(outputFile,f,w,h,b):
     driver = gdal.GetDriverByName(f)
@@ -343,7 +346,7 @@ def run(args):
     print "Apply stetch to raster image(s)."
     print "#==#"
     #==#
-    if bands != 1 and bands !=3:
+    if numberOfBands != 1 and numberOfBands !=3:
         print "The number of bands specified is not expected."
         return 0
     if rows <= 0:
@@ -364,7 +367,7 @@ def run(args):
     #==#
     #Load input image file and breakpoints file
     inputDataset = gdal.Open(inputFile,GA_ReadOnly)
-    lookUpTables = LookUpTables(breakPointsFile)
+    lookUpTables = LookUpTables(breakPointsFile,numberOfBands)
     if inputDataset is None:
         print "Error opening input image file."
         return 0
