b3e6844f678c2797ef9afdf2223970dc59627941
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-10-12 22:21:21 -0400

    Update osm_extracts_update.sh

--- examples/ebola/osm_extracts_update.sh
+++ examples/ebola/osm_extracts_update.sh
@@ -9,16 +9,4 @@ AN=hiu
 AE='HIU_INFO@state.gov'
 TO=360
 #===================#
-DSA=( "monrovia_basic" "monrovia_landuse" "monrovia_medicalcenters" "monrovia_schools" "monrovia_inlandwaters")
-for DS in "${DSA[@]}"
-do
-   python  $BIN/cybergis-script-geogig-osm-sync.py true -v -gs $GS -ws $WS -ds $DS --username $USER --password $PASS -an $AN -ae $AE -to $TO
-done
-#===================#
-DSA=( "kenema_basic" "kenema_landuse" "kenema_medicalcenters" "kenema_schools" "kenema_inlandwaters")
-for DS in "${DSA[@]}"
-do
-   python $BIN/cybergis-script-geogig-osm-sync.py true -v -gs $GS -ws $WS -ds $DS --username $USER --password $PASS -an $AN -ae $AE -to $TO
-done
-#===================#
-echo "Done updating OSM extracts."
+python  $BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts osm_extracts.tsv
