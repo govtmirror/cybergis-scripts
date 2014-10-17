8fb5f857dc0727ac846ccce76e84b2e24ff98b86
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-10-17 16:33:55 -0400

    Update osm_extracts_snapshot.sh

--- examples/khulna/osm_extracts_snapshot.sh
+++ examples/khulna/osm_extracts_snapshot.sh
@@ -22,8 +22,30 @@ PRJ='EPSG:4326'
 WS=<Workspace>
 DS=<Data Store>
 #===================#
-#Khulna, Bangladesh
-LG=khulna_$TIMESTAMP
+#Khulna, Bangladesh / Raw
+LG=khulna_raw_$TIMESTAMP
+FTA=( "khulna_nodes" "khulna_ways")
+SNAPA=()
+STYLESA=( "cybergis_basic_point_blue" "cybergis_basic_line_blue" )
+for FT in "${FTA[@]}"
+do
+    #SNAP=$NS"_"$FT"_"$TIMESTAMP
+    SNAP=$FT"_"$TIMESTAMP
+    SNAPA+=($SNAP)
+    echo "-----------"
+    echo "Snapshoting "$FT" as "$NS"_"$FT"_"$TIMESTAMP
+    $BIN/cybergis-script-pull-wfs.sh $WFS $NS $FT $PRJ $HOST $DB $DB_USER $DB_PASS $SNAP $TEMP
+    python $BIN/cybergis-script-geoserver-publish-layers.py -gs $GS -ws $WS -ds $DS -ft $SNAP --username $GS_USER --password $GS_PASS
+done
+LAYERS=$(printf ",%s" "${SNAPA[@]}")
+LAYERS=$(echo $LAYERS | cut -c 2- )
+STYLES=$(printf ",%s" "${STYLESA[@]}")
+STYLES=$(echo $STYLES | cut -c 2- )
+python $BIN/cybergis-script-geoserver-publish-layergroup.py -gs $GS -ws $WS -lg $LG --layers "$LAYERS" --styles "$STYLES" --username $GS_USER --password $GS_PASS
+#===================#
+#===================#
+#Khulna, Bangladesh / Norma
+LG=khulna_normal_$TIMESTAMP
 FTA=( "khulna_medicalcenters_osm_medical_centers" "khulna_basic_osm_buildings" "khulna_basic_osm_roads" "khulna_schools_osm_schools_areas" "khulna_landuse_osm_landuse_farmland" "khulna_landuse_osm_landuse_residential" "khulna_landuse_osm_landuse_military" "khulna_landuse_osm_landuse_forest" )
 SNAPA=()
 STYLESA=( "cybergis_health_medical_center" "cybergis_structure_buildings" "cybergis_basic_line_blue" "cybergis_education_schools_areas" "cybergis_landuse_farmland" "cybergis_landuse_residential" "cybergis_landuse_military" "cybergis_landuse_forest" )
