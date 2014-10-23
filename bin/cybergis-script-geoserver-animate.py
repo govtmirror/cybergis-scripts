6abaf8718478656eb3dc5bb67c18dc817359c9d6
state-hiu <HIU_INFO@state.gov>
2014-10-22 23:31:45 -0400

    first animation scipt

---
+++ bin/cybergis-script-geoserver-animate.py
@@ -0,0 +1,35 @@
+from base64 import b64encode
+from optparse import make_option
+import json
+import urllib
+import urllib2
+import argparse
+import time
+import sys
+import os
+import subprocess
+#==#
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib', 'cybergis')))
+import gs._geoserver_animate
+#==#
+parser = argparse.ArgumentParser(description='')
+#==#
+parser.add_argument('-gs', '--geoserver', help="The url of the target GeoServer.")
+#==#
+parser.add_argument('--layers', help="The layers to animate through.")
+parser.add_argument('--bbox', help="The bounding box to animate against.")
+#==#
+parser.add_argument('--width', help="The width of the output animated GIF in pixels.  These should be compatible with the bounding box.")
+parser.add_argument('--height', help="The height of the output animated GIF in pixels.  This should be compatible with the bounding box.")
+#==#
+parser.add_argument("--username", help="The username to use for basic auth requests.")
+parser.add_argument("--password", help="The password to use for basic auth requests.")
+#==#
+parser.add_argument('--verbose', '-v', default=0, action='count', help="Print out intermediate status messages.")
+#==#
+parser.add_argument('--url', default=0, action='count', help="Display url to animated GIF")
+parser.add_argument('--file', default=0, action='count', help="Export animated GIF to file.")
+#==#
+args = parser.parse_args()
+#==#
+gs._geoserver_animate.run(args)
---
+++ lib/cybergis/gs/_geoserver_animate.py
@@ -0,0 +1,108 @@
+from base64 import b64encode
+from optparse import make_option
+import json
+import urllib
+import urllib2
+import argparse
+import time
+import os
+import subprocess
+
+def make_request(url, params, auth=None, data=None, contentType=None):
+    """
+    Prepares a request from a url, params, and optionally authentication.
+    """
+    req = urllib2.Request(url + urllib.urlencode(params), data=data)
+
+    if auth:
+        req.add_header('AUTHORIZATION', 'Basic ' + auth)
+    
+    if contentType:
+        req.add_header('Content-type', contentType)
+    else:
+        if data:
+            req.add_header('Content-type', 'text/xml')
+
+    return urllib2.urlopen(req)
+
+def parse_url(url):
+    
+    if (url is None) or len(url) == 0:
+        return None
+    
+    index = url.rfind('/')
+
+    if index != (len(url)-1):
+        url += '/'
+    
+    return url
+
+    
+def buildQueryString(layers, bbox, width, height):
+    qs = ""
+
+    qs += "FORMAT=image/gif;subtype=animated&format_options=gif_loop_continuosly:true&aparam=layers"
+    qs += "&SRS=EPSG%3A900913"
+    qs += "&BBOX="+bbox
+    qs += "&WIDTH="+width
+    qs += "&HEIGHT="+height
+    qs += "&avalues="+layers
+
+    return urllib.urlencode(qs)
+
+def buildURL(geoserver, layers, bbox, width, height):
+    if geoserver:
+        qs = buildQueryString(layers, bbox, width, height)
+        return geoserver + "wms/animate?" + qs
+    else:
+        return None
+
+def parse_layers(layers):
+    if layers and len(layers) > 0:
+        try:
+            return layers.split(",")
+        except:
+            return None
+    else:
+        return None
+
+def run(args):
+    #print args
+    #==#
+    verbose = args.verbose
+    #==#
+    layers = parse_layers(args.layers)
+    geoserver = parse_url(args.geoserver)
+    #==#
+    width = args.width
+    height = args.height
+    #==#
+    bbox = args.bbox
+    #==#
+    export_url = args.url
+    export_file = args.file
+    #==#
+    auth = None
+    if args.username and args.password:
+      auth = b64encode('{0}:{1}'.format(args.username, args.password))
+    #==#
+    print "=================================="
+    print "#==#"
+    print "CyberGIS Script / cybergis-scrit-geoserver-animate.py"
+    print "Animate through list of layers for a given bounding box"
+    print "#==#"
+    #==#
+    if not layers:
+        print "Could not parse layers correctly."
+        return 1
+    
+    #==#
+    #Animate through layers
+    try:
+        url = buildURL(geoserver, layers, bbox, width, height)
+        if export_url:
+            print url
+    except:
+        print "Couldn't animate through layers "+args.layers+"."
+        raise
+    print "=================================="
