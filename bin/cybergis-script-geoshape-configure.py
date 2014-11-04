79bcc3331a0285f13f035834005f832b831b6324
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-11-03 22:08:06 -0500

    Update cybergis-script-geoshape-configure.py

--- bin/cybergis-script-geoshape-configure.py
+++ bin/cybergis-script-geoshape-configure.py
@@ -19,18 +19,19 @@ parser.add_argument("--env", default="standalone", help="The environment (standa
 #==#
 parser.add_argument("--fqdn", default="localhost", help="The fqdn of GeoSHAPE.")
 #==#
-parser.add_argument("--db_host", help="The database host")
-parser.add_argument("--db_ip", help="The database ip")
-parser.add_argument("--db_port", default="5432", help="The database port")
-parser.add_argument("--db_user", default="postgres", help="The database user")
-parser.add_argument("--db_pass", help="The database password")
-#==#
 parser.add_argument("--gs_baseline", default="master", help="The baseline geoserver_data version.")
 #==#
 parser.add_argument('--banner', default=0, action='count', help="Display a banner")
 parser.add_argument("--banner_text", help="The banner text.")
 parser.add_argument("--banner_color_text", help="The foreground/font color of the banner text.")
 parser.add_argument("--banner_color_background", help="The background color of the banner.")
+#==#
+#When env equals application or aws
+parser.add_argument("--db_host", help="The database host")
+parser.add_argument("--db_ip", help="The database ip")
+parser.add_argument("--db_port", default="5432", help="The database port")
+parser.add_argument("--db_user", default="postgres", help="The database user")
+parser.add_argument("--db_pass", help="The database password")
 #==#
 args = parser.parse_args()
 #==#
