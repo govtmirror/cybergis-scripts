e2b1e537b6a35f98dab439b0d546f5e4ae8ef012
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-11-12 17:16:01 -0500

    Update cybergis-script-ittc-stretch.py

--- bin/cybergis-script-ittc-stretch.py
+++ bin/cybergis-script-ittc-stretch.py
@@ -19,8 +19,8 @@ parser.add_argument("breakpoints", help="Path to breakpoints file")
 parser.add_argument("output", help="Path to output image file")
 parser.add_argument("bands", help="The number of bands to stretch.  This is not inferred.  You need to be explicit.  Most likely 1 or 3.")
 #
-parser.add_argument('-r', '--rows', default='256', help="The number of rows to load into memory at a time to stretch.")
-parser.add_argument('-t', '--threads', default='1', help="The maximum number of threads to activate.")
+parser.add_argument('-r', '--rows', default='256', help="The number of rows to load into memory at a time to stretch.  Default = 256.")
+parser.add_argument('-t', '--threads', default='1', help="The maximum number of threads to activate.  Default = 1.")
 #
 parser.add_argument('--verbose', '-v', default=0, action='count', help="Print out intermediate status messages.")
 #
