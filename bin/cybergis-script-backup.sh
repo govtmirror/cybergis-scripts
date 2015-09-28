dbc445d915bff8915991ce0cb5175e9378adb830
Humanitarian Information Unit <HIU_DEV@state.gov>
2015-09-28 14:45:40 -0400

    Create cybergis-script-backup.sh

---
+++ bin/cybergis-script-backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DATE=$(date)
+BACKUP_CMD=$1
+#==================================#
+backup_repos(){
+  echo "backup_repos"
+  if [[ $# -ne 3 ]]; then
+    echo "Usage: cybergis-script-backup.sh repos <manifest> <output>"
+  else
+    BACKUP_CMD=$1
+    MANIFEST=$2
+    OUTPUT=$3
+    #
+    mkdir -p $OUTPUT
+    cd $OUTPUT
+    while read line; do
+        NAME=$(cat $MANIFEST | cut -f1)
+        SOURCE=$(cat $MANIFEST | cut -f2)
+        git clone $SOURCE $NAME
+    done <$MANIFEST
+  fi
+}
+
+if [[ "$BACKUP_CMD" == "repos" ]]; then
+        
+    if [[ $# -ne 3 ]]; then
+        echo "Usage: cybergis-script-backup.sh repos <manifest> <output>"
+    else
+        export -f backup_repos
+        bash --login -c "backup_repos $BACKUP_CMD '${2}' '${3}'"
+    fi
+    
+else
+    echo "Usage: cybergis-script-backup.sh [repos]"
+fi
